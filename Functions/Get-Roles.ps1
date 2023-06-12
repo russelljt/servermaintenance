b7562209a28ae88f6c807c031cf319ad0fd4230b
Jesse <russelljt@gmail.com>
2023-06-12 15:07:22 -0400

    Update Get-Roles.ps1

--- Functions/Get-Roles.ps1
+++ Functions/Get-Roles.ps1
@@ -4,15 +4,6 @@ function Get-Roles {
     $roles = (($includedroles | ForEach-Object {[regex]::Escape($_)}) –join "|")
 
     $installedroles = Get-WindowsFeature | Where-Object {($_.InstallState -eq "Installed") -and ($_.name -match $roles)} | Select-Object Name
-    
-    <#
-    Switch ($installedroles.name) {
-        # If roles are detected, start respective separate maintenance script
-        "AD-Domain-Services" {}
-        "FS-DFS-Replication" {}
-        "Hyper-V" {}
-    }
-    #>
     return $installedroles
 } 
 
